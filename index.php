41f32f6552d9604741c4eb46046663b59b5b3640
farhan <akhtarfarhan281@gmail.com>
2024-09-07 15:04:38 +0530

    Started Learning PHP

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php
+
+echo "Hello world ";
+echo "Code above the html body";
+echo 2024," " ;
+echo "Hello ","Farhan ","in ",2024;
+print "Printing using print method";
+print("Printing using print method inside bracket")
+
+?>
+
+<html>
+<h1>This is my first PHP code</h1>
+<div>
+    <b>
+         <?php 
+    echo "This paragraph inside html div";
+    ?> 
+    </b>
+  
+</div>
+</html>
+
+<?php
+echo "Code after/bottom of html body";
+?>
+
+<?php
+echo "The End";
+?>
